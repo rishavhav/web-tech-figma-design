6700e2dd6f6a130a494bd7bc3e47256406c99689
risha <rs1470@g.rit.edu>
2023-11-18 12:59:41 -0500

    homepage

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById("myButton").onclick = function () {
   location.href = "some_url"
 }
+
+document.getElementById("scrollSlider").addEventListener("input", function () {
+  var container = document.getElementById("boxContainer")
+  var maxScrollLeft = container.scrollWidth - container.clientWidth
+  container.scrollLeft = (this.value / 100) * maxScrollLeft
+})
