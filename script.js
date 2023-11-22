bfbb354fda126dc09dcf552b9de97d3593e5a028
risha <rs1470@g.rit.edu>
2023-11-21 20:55:22 -0500

    blog pages added

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ document.getElementById("scrollSlider").addEventListener("input", function () {
   var maxScrollLeft = container.scrollWidth - container.clientWidth
   container.scrollLeft = (this.value / 100) * maxScrollLeft
 })
+
+function joinMailingList() {
+  var email = "rs1470@rit.edu"
+  var subject = encodeURIComponent("Subscription Request")
+  var emailBody = encodeURIComponent("I want to subscribe to your mailing list!")
+  window.location.href = "mailto:" + email + "?subject=" + subject + "&body=" + emailBody
+}
